361384bdf211fd5dd3ed3f3ecf77deaeee67aeb7
Chris <morenochristopher580@gmail.com>
2025-10-19 13:50:17 -0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Windows;
 using System.Windows.Input;
-using System.Diagnostics;
 using System.Windows.Threading;
 
 namespace NekoMonitor
@@ -9,73 +8,61 @@ namespace NekoMonitor
     public partial class MainWindow : Window
     {
         private DispatcherTimer updateTimer;
-        private PerformanceCounter cpuCounter;
-        private PerformanceCounter ramCounter;
+        private bool isDragging = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            InitializePerformanceCounters();
-            StartMonitoring();
+            StartSafeMonitoring();
         }
 
-        private void InitializePerformanceCounters()
-        {
-            try
-            {
-                cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-                ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-            }
-            catch (Exception ex)
-            {
-                // Fallback if performance counters aren't available
-                CpuText.Text = "CPU: N/A";
-                RamText.Text = "RAM: N/A";
-            }
-        }
-
-        private void StartMonitoring()
+        private void StartSafeMonitoring()
         {
             updateTimer = new DispatcherTimer();
-            updateTimer.Interval = TimeSpan.FromSeconds(1);
-            updateTimer.Tick += UpdateStats;
+            updateTimer.Interval = TimeSpan.FromSeconds(2); // Slower updates = more stable
+            updateTimer.Tick += UpdateStatsSafely;
             updateTimer.Start();
         }
 
-        private void UpdateStats(object sender, EventArgs e)
+        private void UpdateStatsSafely(object sender, EventArgs e)
         {
             try
             {
-                if (cpuCounter != null)
-                {
-                    float cpuUsage = cpuCounter.NextValue();
-                    CpuText.Text = $"CPU: {cpuUsage:F1}%";
-                }
-
-                if (ramCounter != null)
-                {
-                    float ramUsage = ramCounter.NextValue();
-                    RamText.Text = $"RAM: {ramUsage:F1}%";
-                }
-
-                // Simple network and disk placeholders
-                NetText.Text = "NET: 0 KB/s";
-                DiskText.Text = "DISK: 0%";
+                // Simple random demo data instead of PerformanceCounter
+                var random = new Random();
+                CpuText.Text = $"CPU: {random.Next(1, 100)}%";
+                RamText.Text = $"RAM: {random.Next(1, 100)}%";
+                NetText.Text = $"NET: {random.Next(0, 1000)} KB/s";
+                DiskText.Text = $"DISK: {random.Next(1, 100)}%";
             }
             catch
             {
-                // Silent fail - keep showing last values
+                // Silent fail - don't crash
             }
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
+            try
+            {
+                this.DragMove();
+            }
+            catch
+            {
+                // Ignore drag errors
+            }
         }
 
         private void Close_Click(object sender, RoutedEventArgs e)
         {
+            updateTimer?.Stop();
             this.Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            updateTimer?.Stop();
+            base.OnClosed(e);
         }
     }
 }
